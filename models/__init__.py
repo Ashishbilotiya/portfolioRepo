83aa9901216102c0512d239c1fb5260dfcef6fe5
ASHISH BILOTIYA <ashishbilotia2003xyz@gmail.com>
2026-01-13 05:49:17 +0530

    Refactor famous_books path to use BASE_DIR
    Updated the path for loading famous_books.pkl to use a dynamic base directory.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 import pickle 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+famous_books_PATH = os.path.join(BASE_DIR, "famous_books.pkl")
 
 
-famous_books = pickle.load(open("famous_books.pkl","rb"))
+
+famous_books = pickle.load(open(famous_books_PATH,"rb"))
 books = pickle.load(open("books.pkl","rb"))
 books_pt = pickle.load(open("books_pt.pkl","rb"))
 books_similarity_score = pickle.load(open("books_similarity_score.pkl","rb"))
