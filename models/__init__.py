d74c79dbd294a8f371e3e19052de6e2f13ad5f5c
ASHISH BILOTIYA <ashishbilotia2003xyz@gmail.com>
2026-01-13 17:30:09 +0530

    Refactor model loading paths and variables

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,29 @@
 import pickle 
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-famous_books_PATH = os.path.join(BASE_DIR, "famous_books.pkl")
+ASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+famous_books_MODEL_PATH = os.path.join(BASE_DIR, "famous_books.pkl")
+books_MODEL_PATH = os.path.join(BASE_DIR, "books.pkl")
+books_pt_MODEL_PATH = os.path.join(BASE_DIR, "books_pt.pkl")
+books_similarity_score_MODEL_PATH = os.path.join(BASE_DIR, "books_similarity_score.pkl")
+movies_MODEL_PATH = os.path.join(BASE_DIR, "movies.pkl")
+movies_similarity_score_MODEL_PATH = os.path.join(BASE_DIR, "movies_similarity_score.pkl")
+
+famous_books = pickle.load(open(famous_books_MODEL_PATH,"rb"))
+books = pickle.load(open(books_MODEL_PATH,"rb"))
+books_pt = pickle.load(open(books_pt_MODEL_PATH,"rb"))
+books_similarity_score = pickle.load(open(books_similarity_score_MODEL_PATH,"rb"))
+movies = pickle.load(open(movies_MODEL_PATH,"rb"))
+movies_similarity_score = pickle.load(open(movies_similarity_score_MODEL_PATH,"rb"))
+
+# print("Models Loaded Successfully")
 
 
 
-famous_books = pickle.load(open(famous_books_PATH,"rb"))
-books = pickle.load(open("books.pkl","rb"))
-books_pt = pickle.load(open("books_pt.pkl","rb"))
-books_similarity_score = pickle.load(open("books_similarity_score.pkl","rb"))
-movies = pickle.load(open("movies.pkl","rb"))
-movies_similarity_score = pickle.load(open("movies_similarity_score.pkl","rb"))
+# famous_books = pickle.load(open(MODEL_PATH,"rb"))
+# books = pickle.load(open("books.pkl","rb"))
+# books_pt = pickle.load(open("books_pt.pkl","rb"))
+# books_similarity_score = pickle.load(open("books_similarity_score.pkl","rb"))
+# movies = pickle.load(open("movies.pkl","rb"))
+# movies_similarity_score = pickle.load(open("movies_similarity_score.pkl","rb"))
